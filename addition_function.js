b21945472d0f1bfefaa4e77e4d9b4f49bc77d6db
ezchuang <129792088+ezchuang@users.noreply.github.com>
2023-10-26 07:03:46 +0800

    add test route

---
+++ addition_function.js
@@ -0,0 +1,5 @@
+function addition_function(){
+    console.log("addition")
+}
+
+module.exports.addition_function = addition_function;
--- app.js
+++ app.js
@@ -1,2 +1,38 @@
 const express = require("express");
 const app = express();
+const bp = express("body-parser")
+
+const addition_function = require("./addition_function")
+
+
+// middleware
+app.use(bp.static("public"))
+app.use(bp.json())
+app.use(bp.urlencoded({ extended: true })) // 處理 POST Body，extended 為擴展功能，容許 String 以外的資料
+
+app.use((err, req, res, next) => {
+    if (err){
+        console.log(err)
+        return 
+    }
+    next()
+})
+
+
+// routes
+app.get("/", (req, res) => {
+    console.log(`${req.url} ${req.body}`)
+    return 
+})
+
+app.get("/newPost", (req, res) => {
+    console.log(`${req.url}/index.html`)
+    res.sendFile(`${__dirname}/index.html`)
+    return 
+})
+
+
+
+app.listen(3000, () =>{
+    console.log("server start")
+})
